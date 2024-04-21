649b9adb0172a72cd956e7212f4dd3ab7a1f8124
ajnanmvr <ajnanpulikkathody@gmail.com>
2024-04-21 20:50:52 +0530

    jay jay

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -410,9 +410,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               {/* <!-- Menu Item Chart --> */}
               <li>
                 <Link
-                  href="/chart"
+                  href="/analytics"
                   className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-                    pathname.includes("chart") && "bg-graydark dark:bg-meta-4"
+                    pathname.includes("analytics") && "bg-graydark dark:bg-meta-4"
                   }`}
                 >
                   <svg
@@ -444,7 +444,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                       </clipPath>
                     </defs>
                   </svg>
-                  Chart
+                  Analytics
                 </Link>
               </li>
               {/* <!-- Menu Item Chart --> */}
