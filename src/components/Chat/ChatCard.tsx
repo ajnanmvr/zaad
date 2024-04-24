015f78f67510e8dfd57d688878868d32419fb71a
ajnanmvr <ajnanpulikkathody@gmail.com>
2024-04-24 15:04:42 +0530

    olakkathakka thaka

--- src/components/Chat/ChatCard.tsx
+++ src/components/Chat/ChatCard.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Chat } from "@/types/chat";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { TEmployeeList } from "@/libs/types";
+import clsx from "clsx";
 
 const chatData: Chat[] = [
   {
@@ -54,54 +58,58 @@ const chatData: Chat[] = [
 ];
 
 const ChatCard = () => {
+  const [employees, setEmployees] = useState<TEmployeeList>([{
+    id: "",
+    name: "",
+    expiryDate: "",
+    docs: 0,
+    status: "",
+    company: { id: "", name: "" }
+  }])
+  const fetchData = async () => {
+    try {
+      const data = await axios.get("/api/employee")
+      setEmployees(data.data.data.slice(0, 5))
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
   return (
     <div className="col-span-12 rounded-sm border border-stroke bg-white py-6 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-4">
       <h4 className="mb-6 px-7.5 text-xl font-semibold text-black dark:text-white">
-        Chats
+        Emloyees
       </h4>
 
       <div>
-        {chatData.map((chat, key) => (
+        {employees.map((employee, key) => (
           <Link
             href="/"
-            className="flex items-center gap-5 px-7.5 py-3 hover:bg-gray-3 dark:hover:bg-meta-4"
+            className="flex capitalize items-center gap-5 px-7.5 py-3 hover:bg-gray-3 dark:hover:bg-meta-4"
             key={key}
           >
-            <div className="relative h-14 w-14 rounded-full">
-              <Image
-                width={56}
-                height={56}
-                src={chat.avatar}
-                alt="User"
-                style={{
-                  width: "auto",
-                  height: "auto",
-                }}
-              />
-              <span
-                className={`absolute bottom-0 right-0 h-3.5 w-3.5 rounded-full border-2 border-white ${
-                  chat.dot === 6 ? "bg-meta-6" : `bg-meta-${chat.dot}`
-                } `}
-              ></span>
-            </div>
+            <div
+              className={clsx(employee.status === "renewal" ? "bg-meta-6": employee.status === "expired" ? "bg-red": `bg-meta-3`, "h-3.5 w-3.5 rounded-full border-2 border-white")}
+            ></div>
 
             <div className="flex flex-1 items-center justify-between">
               <div>
                 <h5 className="font-medium text-black dark:text-white">
-                  {chat.name}
+                  {employee.name}
                 </h5>
                 <p>
                   <span className="text-sm text-black dark:text-white">
-                    {chat.text}
+                    {employee.company.name}
                   </span>
-                  <span className="text-xs"> . {chat.time} min</span>
+                  <span className="text-xs"> . {employee.status}</span>
                 </p>
               </div>
-              {chat.textCount !== 0 && (
+              {employee.docs !== 0 && (
                 <div className="flex h-6 w-6 items-center justify-center rounded-full bg-primary">
                   <span className="text-sm font-medium text-white">
-                    {" "}
-                    {chat.textCount}
+                    {employee.docs}
                   </span>
                 </div>
               )}
