4580e38d9b70ffc19246585b88b648acf778fe6e
ajnanmvr <ajnanpulikkathody@gmail.com>
2024-07-16 11:50:56 +0530

    nthing

--- src/components/Tables/TransactionList.tsx
+++ src/components/Tables/TransactionList.tsx
@@ -216,14 +216,14 @@ const TransactionList = ({ type, id }: {
 
             <Link
 
-              href={"transactions/income"}
+              href={"/accounts/transactions/income"}
               className="inline-flex items-center justify-center rounded-md bg-meta-3 px-4 py-1 text-center font-medium text-white hover:bg-opacity-90"
             >
               <svg xmlns="http://www.w3.org/2000/svg" className="w-3 mr-1 h-3 fill-white" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" /></svg>
               Income
             </Link>
             <Link
-              href={"transactions/expense"}
+              href={"/accounts/transactions/expense"}
               className="inline-flex items-center justify-center rounded-md bg-red px-4 py-1 text-center font-medium text-white hover:bg-opacity-90"
             ><svg xmlns="http://www.w3.org/2000/svg" className="w-3 mr-1 h-3 fill-white" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" /></svg>
               Expense
