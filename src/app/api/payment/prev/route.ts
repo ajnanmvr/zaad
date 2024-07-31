3dd6131c12e2878d933615aa2a16ae078e039ee0
ajnanmvr <ajnanpulikkathody@gmail.com>
2024-07-31 19:38:44 +0530

    new number setting

--- src/app/api/payment/prev/route.ts
+++ src/app/api/payment/prev/route.ts
@@ -10,10 +10,7 @@ export async function GET(request: Request) {
     })
       .sort({ createdAt: -1 })
       .select("suffix number");
-    return Response.json(
-      { suffix, number: (number || 0) + 1 },
-      { status: 201 }
-    );
+    return Response.json({ suffix, number: number + 1 || 0 }, { status: 201 });
   } catch (error) {
     return Response.json(error, { status: 401 });
   }
