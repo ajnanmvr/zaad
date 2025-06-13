3b85a5b9effff963ff092822c0c1395713c5d4d3
ajnanmvr <ajnanpulikkathody@gmail.com>
2025-06-13 21:23:38 +0530

    Refactor logout process to utilize NextResponse for consistency; remove manual cookie clearing logic and enhance user feedback during logout.

--- src/app/api/users/auth/logout/route.ts
+++ src/app/api/users/auth/logout/route.ts
@@ -1,14 +1,27 @@
 import { NextResponse } from "next/server";
+
 export async function GET() {
   try {
-    const response = NextResponse.json({
-      message: "Logout Successful",
-      success: true,
-    });
+    const response = new NextResponse(
+      JSON.stringify({
+        message: "Logout Successful",
+        success: true,
+      }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
     response.cookies.set("auth", "", { httpOnly: true, expires: new Date(0) });
-    response.cookies.set("partner", "", { httpOnly: true, expires: new Date(0) });
+    response.cookies.set("partner", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    });
     return response;
   } catch (error: any) {
-    return Response.json({ message: "Logout Failed", error }, { status: 500 });
+    return NextResponse.json(
+      { message: "Logout Failed", error },
+      { status: 500 }
+    );
   }
 }
